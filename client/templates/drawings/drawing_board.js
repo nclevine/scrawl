a0e9e7bbb8e4b61bfebb95dbafded997b61be649
Noah Levine <nclevine@gmail.com>
2015-05-31 15:14:43 -0400

    drawings are synced somewhat but not updating automatically

--- client/templates/drawings/drawing_board.js
+++ client/templates/drawings/drawing_board.js
@@ -3,21 +3,41 @@ Template.body.onCreated(function(){
 })
 
 Template.drawingBoard.onRendered(function(){
-  paper.setup('canvas');
-  project.currentStyle = {
+  var saved = new Project('saved');
+  var main = new Project('canvas');
+  drawing = this.data;
+  loadedData = drawing.drawingData;
+  saved.importJSON(loadedData);
+  main.activate()
+  main.currentStyle = {
     strokeColor: 'black'
   }
-  var tool = new Tool();
-  tool.fixedDistance = 10;
+  var pencil = new Tool();
+  pencil.fixedDistance = 10;
   var path;
-  tool.onMouseDown = function(event){
-    console.log('mouse clicked!');
+  pencil.onMouseDown = function(event){
     path = new Path();
     path.add(event.point);
     path.strokeColor = 'black';
   }
-  tool.onMouseDrag = function(event){
-    console.log('mouse dragged!');
+  pencil.onMouseDrag = function(event){
     path.add(event.point);
   }
+  pencil.onMouseUp = function(event){
+    var json = main.exportJSON();
+    saved.importJSON(json);
+    main.clear();
+    saved.view.update();
+    var drawingData = saved.exportJSON();
+    Session.set('paths', drawingData);
+    Meteor.call('drawingUpdate', drawing._id, drawingData, function(error, result){
+      console.log(result);
+    });
+    main.activate();
+  }
+
+  Tracker.autorun(function () {
+    
+  });
+
 });
