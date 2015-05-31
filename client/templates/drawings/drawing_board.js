f119ef13ed7d2c51aee2d88eefd84a4b8f8dae07
Noah Levine <nclevine@gmail.com>
2015-05-31 19:45:52 -0400

    fixed templating

--- client/templates/drawings/drawing_board.js
+++ client/templates/drawings/drawing_board.js
@@ -3,13 +3,13 @@ Template.body.onCreated(function(){
 })
 
 Template.drawingBoard.onRendered(function(){
-  var saved = new Project('saved');
-  var main = new Project('canvas');
+  var saved = new Project('saved-canvas');
+  var working = new Project('working-canvas');
   drawing = this.data;
   loadedData = drawing.drawingData;
   saved.importJSON(loadedData);
-  main.activate()
-  main.currentStyle = {
+  working.activate()
+  working.currentStyle = {
     strokeColor: 'black'
   }
   var pencil = new Tool();
@@ -24,20 +24,15 @@ Template.drawingBoard.onRendered(function(){
     path.add(event.point);
   }
   pencil.onMouseUp = function(event){
-    var json = main.exportJSON();
+    var json = working.exportJSON();
     saved.importJSON(json);
-    main.clear();
+    working.clear();
     saved.view.update();
     var drawingData = saved.exportJSON();
     Session.set('paths', drawingData);
     Meteor.call('drawingUpdate', drawing._id, drawingData, function(error, result){
       console.log(result);
     });
-    main.activate();
+    working.activate();
   }
-
-  Tracker.autorun(function () {
-    
-  });
-
 });
