d47340662f4061fba19c7e22ca0f59d6824f2667
Noah Levine <nclevine@gmail.com>
2015-06-03 14:20:03 -0400

    added shake functionality, optimizing for mobile

--- client/templates/drawings/drawing_controls.js
+++ client/templates/drawings/drawing_controls.js
@@ -1,17 +1,42 @@
+var shakeShowTools;
+
 Template.drawingControls.onRendered(function(){
   colors = new Project('color-picker');
   var colorWheel = new Raster('color-wheel');
   colorWheel.position = colors.view.center;
-  colorWheel.scale(1, 0.5);
+  // colorWheel.scale(1, 0.5);
   colorPicker = new Tool()
   colorPicker.onMouseDown = pickColor;
   colorPicker.onMouseDrag = pickColor;
   function pickColor(event){
     var color = colorWheel.getPixel(event.point);
+    console.log(colors.view.bounds);
     $('.choose-color').css('backgroundColor', color.toCSS());
     working.currentStyle.strokeColor = color;
   };
-})
+
+  shakeShowTools = new Shake({
+    threshold: 15
+  });
+  shakeShowTools.start();
+  window.addEventListener('shake', shakeDetector);
+  function shakeDetector(event){
+    event.preventDefault();
+    if($('.drawing-controls').hasClass('closed')){
+      $('.drawing-controls').css('display', 'block');
+      $('.exit-drawing').css('display', 'block');
+    } else{
+      $('.drawing-controls').css('display', 'none');
+      $('.exit-drawing').css('display', 'none');
+    };
+    $('.drawing-controls').toggleClass('closed');
+    $('.exit-drawing').toggleClass('closed');
+  };
+});
+
+Template.drawingControls.onDestroyed(function(){
+  shakeShowTools.stop();
+});
 
 Template.drawingControls.events({
   'click .undo': function(event){
